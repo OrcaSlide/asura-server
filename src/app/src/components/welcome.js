352536c16fee9cf49f1192062f21b6474669333f
The Kolibri <kolibri@the-kolibri.com>
2019-02-07 13:31:07 -0600

    ADD: New styles

--- src/app/src/components/welcome.js
+++ src/app/src/components/welcome.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Welcome = (props) => (
-    <h1 className="Welcome">Asura Server</h1>
+    
+    <section className="Welcome">
+        <h1 className="Welcome__Title">Asura Server</h1>
+        <small className="Welcome__Version">v0.0.1</small>
+    </section>
+
+    
 );
 
 export default Welcome;
